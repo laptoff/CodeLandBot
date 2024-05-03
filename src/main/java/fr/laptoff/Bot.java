0ce633462b85b6256bfc1cf3a9569fdc6f255e50
laptoff <laptoff.contact@gmail.com>
2024-05-04 00:48:17 +0200

    Initialise le projet avec la commande §Ping.

---
+++ src/main/java/fr/laptoff/Bot.java
@@ -0,0 +1,10 @@
+package fr.laptoff;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class Main {
+
+    private static final JDA bot = JDABuilder.createDefault("MTIzNjA1Nzk2MTkzNjQ1Nzc2OQ.GbLm2J.9-FgUWISsuR5mGlS6mIXJQuylrjpTR_vlyXnFA").build();
+
+}
---
+++ src/main/java/fr/laptoff/listeners/MessagesListener.java
@@ -0,0 +1,2 @@
+package fr.laptoff.listeners;public class MessagesListener {
+}
