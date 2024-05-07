60e1a68ffed35aeb9cc8bda80eca3f90ee242a63
laptoff <laptoff.contact@gmail.com>
2024-05-08 00:02:23 +0200

    Ajout du menu GitHub.

--- src/main/java/fr/laptoff/listeners/StringSelectInteraction.java
+++ src/main/java/fr/laptoff/listeners/StringSelectInteraction.java
@@ -1,7 +1,12 @@
 package fr.laptoff.listeners;
 
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.components.selections.SelectOption;
+import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu;
 import net.dv8tion.jda.api.interactions.components.text.TextInput;
 import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
 import net.dv8tion.jda.api.interactions.modals.Modal;
@@ -11,9 +16,32 @@ public class StringSelectInteraction extends ListenerAdapter {
 
     @Override
     public void onStringSelectInteraction(@NotNull StringSelectInteractionEvent event){
-        if (event.getComponentId().equals("menu-selector")){
 
+        //GitHub - CodeLand
+        if (event.getComponentId().equals("menu-selector")){
             if (event.getValues().getFirst().equals("github")){
+
+                MessageEmbed embed = new EmbedBuilder()
+                        .setTitle("GitHub - CodeLand")
+                        .setDescription("Menu GitHub de CodeLand.\n Ici, vous pouvez gérer votre compte GitHub ainsi que vos repositories !")
+                        .setFooter(event.getUser().getName())
+                        .build();
+
+                event.replyEmbeds(embed).addActionRow(
+                        StringSelectMenu.create("github-selector")
+                                .addOptions(SelectOption.of("Connexion", "connexion")
+                                        .withDescription("Connectez votre compte GitHub à CodeLand.")
+                                        .withEmoji(Emoji.fromUnicode("U+1F517")))
+                                .build()
+                ).queue();
+
+            }
+        }
+
+        //Modal Connexion GitHub.
+        if (event.getComponentId().equals("github-selector")){
+
+            if (event.getValues().getFirst().equals("connexion")){
                 TextInput input = TextInput.create("github-token", "Token", TextInputStyle.PARAGRAPH)
                         .setPlaceholder("GitHub Token...")
                         .setRequiredRange(5, 1000)
